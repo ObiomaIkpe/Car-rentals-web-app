2dd0749184a8eff65bfba51d3427b3f965d6b0f6
ObiomaIkpe <obiomaikpe1.com>
2023-10-01 11:37:25 -0500

    completed the basic frontend for the frontend

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const path = require('path');
 const express = require('express');
-const mainRouter = require('./routes/home/mainRoutes')
-// import { engine } from 'express-handlebars';
-const exphb = require('express-handlebars')
-const engine = exphb.engine()
+const mainRoutes = require('./routes/home/mainRoutes');
+const adminRoutes = require('./routes/admin/adminRoutes');
+
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public' )));
+
+// import { engine } from 'express-handlebars';
+//set the view engine
+const exphb = require('express-handlebars')
+const engine = exphb.engine()
 app.engine('handlebars', engine);
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.use('/', mainRouter);
+
+
+app.use(express.static(path.join(__dirname, 'public' )));
+
+//use routes
+app.use('/', mainRoutes);
+app.use('/admin', adminRoutes);
+
 
    
 module.exports = app
--- routes/home/mainRoutes.js
+++ routes/home/mainRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('*', (req, res, next) => {
+    req.app.locals.layout = 'main';
+    next();
+})
+
+
 router.get('/', (req, res) => {
     res.render('home/index')
 })
